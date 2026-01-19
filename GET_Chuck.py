9d2682761ac375a18ad1e8092e27eb3050e0a399
Роман <roman.v.antonnikov@gmail.com>
2026-01-19 18:53:48 +0300

    Выполнено ДЗ: Отправка запроса с методом Get, без использования ООП

--- GET_Chuck.py
+++ GET_Chuck.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "https://api.chucknorris.io/jokes/random"
-print(url)  # выводим на печать URL,по которому отправляем запрос
+print(url)  # выводим на печать URL, по которому отправляем запрос
 result = requests.get(url)
 print("Статус код: " + str(result.status_code))
 assert 200 == result.status_code, f"Ошибка! Статус код: {result.status_code}"  # проводим проверку статус кода запроса
