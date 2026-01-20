385c7a95663b4b63060e29762c86890713fb01d5
Роман <roman.v.antonnikov@gmail.com>
2026-01-20 10:12:27 +0300

    Выполнено ДЗ: Отправка запроса с методом Get, без использования ООП

--- GET_Chuck.py
+++ GET_Chuck.py
@@ -5,7 +5,6 @@ print(url)  # выводим на печать URL, по которому отп
 result = requests.get(url)
 print("Статус код: " + str(result.status_code))
 assert 200 == result.status_code, f"Ошибка! Статус код: {result.status_code}"  # проводим проверку статус кода запроса
-if result.status_code == 200:  # проводим проверку ОР и ФР ответа
-    print("Успешно, статус код верен!")
+print("Успешно, статус код верен!")
 result.encoding = "utf-8"  # приводим ответ к правильному виду благодаря указанию кодировки
 print(result.text)
